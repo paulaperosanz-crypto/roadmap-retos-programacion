ff80206fac46f6b46ce73b45e9ef56c665cce749
paulaperosanz-crypto <paulaperosanz@gmail.com>
2025-12-12 13:44:56 +0100

    Create paulaperosanz.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/paulaperosanz.py
@@ -0,0 +1,32 @@
+# https://python.org 
+
+# Esto es un comentario en una línea
+
+ """
+ Y esto es un comentario
+ en varias
+ líneas che
+ """
+
+'''
+bueno esto 
+tambiei es un
+maldito comentario 
+en variai linea
+'''
+
+# Ya acabo este ejercicio pero me voy a tener que ir a aprender la sintaxis porque no se escribir bien
+
+variable_uno = "papitas fritas"
+variable_uno = "diplomado en ingenieria aeronautica"
+
+MY_CONSTANT = "Cocó"  # por convención que no lo cambies
+
+my_int = 7  #podria ponerle cuaquier nombre? si, como edad, o número_de_looney_tunes. Y son variables recuerda.
+my_float = 1.5
+my_str = "Caillou mola"
+my_bool = True
+my_bool_dos = False
+my_other_string = 'Caillou calvo pero con comillas simples'
+
+print("Hola, Pychón!")
