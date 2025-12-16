12e6ec3f53c998d43bf1162eeda761cc1203ade2
paulaperosanz-crypto <paulaperosanz@gmail.com>
2025-12-16 16:11:35 +0100

    paulaperosanz.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/paulaperosanz.py
@@ -0,0 +1,108 @@
+"""
+Operadores de pichón
+"""
+
+# Operadores aritméticos
+print(f"Suma: 10 + 3 = {10 + 3}")
+print(f"Resta: 10 - 3 = {10 - 3}")
+print(f"Multiplicación: 10 * 3 = {10 * 3}")
+print(f"División: 10 / 3 = {10 / 3}")
+print(f"División entera: 10 // 3 = {10 // 3}")
+print(f"Módulo: 10 % 3 = {10 % 3}")
+print(f"Potencia: 10 ** 3 = {10 ** 3}")
+
+# Operadores de comparación
+print("Igualdad: 10 == 3 es {10 == 3}")
+print("Desigualdad: 10 != 3 es {10 != 3}")
+print("Mayor que: 10 > 3 es {10 > 3}")
+print("Menor que: 10 < 3 es {10 < 3}")
+print("Mayor o igual que: 10 >= 3 es {10 >= 3}")
+print("Menor o igual que: 10 <= 3 es {10 <= 3}")
+
+# Operadores lógicos
+print("AND: 3 + 4 = 8 and 2 - 7 = 2 es {3 + 4 = 8 and 2 - 7 = 2}")
+print("OR: 3 + 4 = 8 or 2 - 7 = 2 es {3 + 4 = 8 or 2 - 7 = 2}")
+print("NOT: 3 + 4 = 8 not 2 - 7 = 2 es {3 + 4 = 8 not 2 - 7 = 2}")
+
+# Operadores de asignación
+pepinillos = 1  # asignación (ahora su valor es 1)
+print(pepinillos)
+pepinillos += 4  # suma y asignación (ahora su valor es 5)
+print(pepinillos)
+pepinillos -= 1  # resta y asignación (ahora su valor es 4)
+print(pepinillos)
+pepinillos *= 3  # multiplicación y asignación (ahora su valor es 12)
+print(pepinillos)
+pepinillos /= 2  # división y asignación (ahora su valor es 6)
+print(pepinillos)
+pepinillos %= 2  # módulo y asignación (ahora su valor es 0)
+print(pepinillos)
+pepinillos **= 2  # exponencia y asignación (ahora su valor es 0)
+print(pepinillos)
+pepinillos //= 2  # división entera y asignación (ahora su valor es 0)
+print(pepinillos)
+
+# Operadores de identidad
+NANA = 1
+print(f"NANA is pepinillos es [NANA is pepinillos}")
+print(f"NANA is not pepinillos es [NANA is not pepinillos}")
+
+# Operadores de membresía
+
+print(f"'u' in 'Pauleta' = {'u' in 'Pauleta'}")
+print(f"'u' not in 'Pauleta' = {'u' not in 'Pauleta'}")
+
+# Operadores bit a bit 
+a = 10  # 1010
+b = 3  # 0011
+print(f"AND: 10 & 3 = {10 & 3}")  # 0010
+print(f"OR: 10 | 3 = {10 | 3}")  # 1011
+print(f"XOR: 10 ^ 3 = {10 ^ 3}")  # 1001
+print(f"NOT: 10 ~ 3 = {~3}")  # 1100
+print(f"Desplazamiento a la derecha: 10 >> 2 = {10 >> 2}")  # 0010
+print(f"Desplazamiento a la izquierda: 10 << 2 = {10 << 2}")  # 101000
+
+"""
+Estructuras de control
+"""
+
+# Condicionales
+chocolate = "con leche"
+
+if chocolate == "negro":
+  print("chocolate es 'negro'")
+elif chocolate == "con leche"
+print ("choclate es 'con leche'")
+else:
+print("chocolate no es 'negro' ni 'con leche'")
+
+# Iterativas
+for i in range (3, 7):
+  print(i)
+
+j = 0
+while j <= 10:
+  print(i)
+  i += 1
+
+# Menejo de excepciones
+try:
+  print(10/0)
+except:
+  print("Se ha producido un error")
+finally:
+  print("Ha finalizado el manejo de excepciones")
+
+
+# Extra
+
+for number in range(10, 56):
+  if number % 2 = 0 and != 16 and number % 3 != 0:
+  print(number)
+
+
+
+
+
+
+ 
